c6f4e3e07cf6aeac181d24191e80b45761bec8ac
James Swineson <github@public.swineson.me>
2023-04-22 18:27:13 +0800

    option to set io priority to low

--- RecoilStarter/Program.cs
+++ RecoilStarter/Program.cs
@@ -8,13 +8,15 @@ namespace RecoilStarter
     {
         static void Main(string[] args)
         {
+            var backgroundProcessing = false;
+
             // NVMe (PCIe 4.0 x4) SSD
-            var game_dir = "C:\\temp_storage_test\\Genshin Impact Game";
+            var gameDir = "C:\\temp_storage_test\\Genshin Impact Game";
             var pipeFatness = 4194304; // 8000MiB/s * 500μs
             var randomAccessPreference = 256;
 
             // NVMe (PCIe 3.0 x4) SSD
-            //var game_dir = "D:\\Program Files\\Genshin Impact\\Genshin Impact Game";
+            //var gameDir = "D:\\Program Files\\Genshin Impact\\Genshin Impact Game";
             //var pipeFatness = 1835008; // 3500MiB/s * 500μs
             //var randomAccessPreference = 256;
 
@@ -25,29 +27,45 @@ namespace RecoilStarter
             // (TBD)
 
             // NAS with ZFS RAIDZ2 HDD, over 10Gbps local network
-            //var game_dir = "\\\\li-nas01\\public\\dropbox\\temp_storage_test\\Genshin Impact Game";
+            //var gameDir = "\\\\li-nas01\\public\\dropbox\\temp_storage_test\\Genshin Impact Game";
             //var pipeFatness = 550502; // 525MiB/s * 1ms
             //var randomAccessPreference = 256;
 
-            var hasher = new ManagedFileHasher(game_dir, pipeFatness, randomAccessPreference);
+            if (backgroundProcessing)
+            {
+                // set CPU and IO priority to low so that we don't disturb other programs during hashing
+                using (Process p = Process.GetCurrentProcess()) p.PriorityClass = ProcessPriorityClass.BelowNormal;
+                var ioPrio = (int)IOPriority.Low;
+                Win32.NtSetInformationProcess(-1, PROCESS_INFORMATION_CLASS.ProcessIoPriority, ref ioPrio, 4);
+            }
+
+            var hasher = new ManagedFileHasher(gameDir, pipeFatness, randomAccessPreference);
             Console.Error.WriteLine("[*] Starting hash threads...");
             var watch = Stopwatch.StartNew();
             hasher.Run();
             watch.Stop();
             Console.Error.WriteLine("[+] Hasher returned.");
 
-            // system info
+            if (backgroundProcessing)
+            {
+                // reset CPU and IO priority
+                using (Process p = Process.GetCurrentProcess()) p.PriorityClass = ProcessPriorityClass.Normal;
+                var ioPrio = (int)IOPriority.Normal;
+                Win32.NtSetInformationProcess(-1, PROCESS_INFORMATION_CLASS.ProcessIoPriority, ref ioPrio, 4);
+            }
+
+            // print system info
             // Note: for .net Framework 4.x, Environment.Version always return 4.0.xxxxx.yyyyy;
             // https://learn.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
             ThreadPool.GetMaxThreads(out int workerThreadsCount, out int ioThreadsCount);
             Console.Error.WriteLine(string.Format("[i] OS: {0}", Environment.OSVersion.ToString()));
             Console.Error.WriteLine(string.Format("[i] CLR Version: {0}, Workers: {1}, IO threads: {2}", Environment.Version.ToString(), workerThreadsCount, ioThreadsCount));
 
-            // performance
-            Console.Error.WriteLine(string.Format("[i] Directory: {0}", game_dir));
-            Console.Error.WriteLine(string.Format("[i] Total files: {0}, Total size: {1}bytes", hasher.FileCount, hasher.FileSize));
+            // print performance statistics
+            Console.Error.WriteLine(string.Format("[i] Directory: {0}", gameDir));
+            Console.Error.WriteLine(string.Format("[i] Total files: {0}, Total size: {1:0.00}MiB", hasher.FileCount, (double)hasher.FileSize / 1048576));
             Console.Error.WriteLine(string.Format("[i] Time: {0:0.00}s", (double)watch.ElapsedMilliseconds / 1000));
-            Console.Error.WriteLine(string.Format("[i] Speed: {0:0.00}files/s, {1:0.00}Bytes/s", (double)hasher.FileCount / watch.ElapsedMilliseconds * 1000, (double)hasher.FileSize / watch.ElapsedMilliseconds * 1000));
+            Console.Error.WriteLine(string.Format("[i] Speed: {0:0.00}files/s, {1:0.00}MiB/s", (double)hasher.FileCount / watch.ElapsedMilliseconds * 1000, (double)hasher.FileSize / watch.ElapsedMilliseconds * 1000 / 1048576));
 
             if (Debugger.IsAttached)
             {
---
+++ RecoilStarter/Win32.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RecoilStarter
+{
+    public enum PROCESS_INFORMATION_CLASS : int
+    {
+        ProcessBasicInformation = 0,
+        ProcessQuotaLimits,
+        ProcessIoCounters,
+        ProcessVmCounters,
+        ProcessTimes,
+        ProcessBasePriority,
+        ProcessRaisePriority,
+        ProcessDebugPort,
+        ProcessExceptionPort,
+        ProcessAccessToken,
+        ProcessLdtInformation,
+        ProcessLdtSize,
+        ProcessDefaultHardErrorMode,
+        ProcessIoPortHandlers,
+        ProcessPooledUsageAndLimits,
+        ProcessWorkingSetWatch,
+        ProcessUserModeIOPL,
+        ProcessEnableAlignmentFaultFixup,
+        ProcessPriorityClass,
+        ProcessWx86Information,
+        ProcessHandleCount,
+        ProcessAffinityMask,
+        ProcessPriorityBoost,
+        ProcessDeviceMap,
+        ProcessSessionInformation,
+        ProcessForegroundInformation,
+        ProcessWow64Information,
+        ProcessImageFileName,
+        ProcessLUIDDeviceMapsEnabled,
+        ProcessBreakOnTermination,
+        ProcessDebugObjectHandle,
+        ProcessDebugFlags,
+        ProcessHandleTracing,
+        ProcessIoPriority,
+        ProcessExecuteFlags,
+        ProcessResourceManagement,
+        ProcessCookie,
+        ProcessImageInformation,
+        ProcessCycleTime,
+        ProcessPagePriority,
+        ProcessInstrumentationCallback,
+        ProcessThreadStackAllocation,
+        ProcessWorkingSetWatchEx,
+        ProcessImageFileNameWin32,
+        ProcessImageFileMapping,
+        ProcessAffinityUpdateMode,
+        ProcessMemoryAllocationMode,
+        MaxProcessInfoClass
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PROCESS_BASIC_INFORMATION
+    {
+        public int ExitStatus;
+        public int PebBaseAddress;
+        public int AffinityMask;
+        public int BasePriority;
+        public int UniqueProcessId;
+        public int InheritedFromUniqueProcessId;
+        public int Size
+        {
+            get { return 24; }
+        }
+    };
+
+    public enum IOPriority
+    {
+        Critical = 4,
+        High = 3,
+        Low = 1,
+        Normal = 2,
+        VeryLow = 0,
+    };
+
+    class Win32
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern int NtSetInformationProcess(int processHandle, PROCESS_INFORMATION_CLASS processInformationClass, ref int processInformation, uint processInformationLength);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern int NtQueryInformationProcess(int processHandle, PROCESS_INFORMATION_CLASS processInformationClass, ref int processInformation, int processInformationLength, ref int returnLength);
+    }
+}
